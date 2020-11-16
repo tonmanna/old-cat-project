5df9820e5d8c68e5167ddda44c67f92fb93bc757
Worawut Boontan <worawut@theiconweb.com>
2020-11-16 21:35:34 +0700

    Fix View Change detection

--- apps/ng-app/src/app/app.component.ts
+++ apps/ng-app/src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable, of, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { sendToNG$, sendToElm } from './global';
-import { PlayerListService, RulesPlayer, removeVowels } from './player.service'
+import { PlayerListService, RulesPlayer, removeVowels } from './player.service';
 @Component({
   selector: 'code-cat-root',
   templateUrl: './app.component.html',
@@ -13,33 +13,42 @@ export class AppComponent implements OnInit, OnDestroy {
   myForm: FormGroup;
   destroy$ = new Subject();
   playerList$ = new Observable<RulesPlayer[]>();
-  constructor(private playerListService : PlayerListService) {
+  constructor(
+    private playerListService: PlayerListService,
+    private _cdr: ChangeDetectorRef
+  ) {
     this.myForm = new FormGroup({});
   }
+
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.unsubscribe();
   }
 
   ngOnInit() {
-    sendToNG$.pipe(takeUntil(this.destroy$)).subscribe((value)=>{
-      console.log('value: ', value);
-      if(value){
-        const player = { name : value, withoutVowelsName : removeVowels(value)} as RulesPlayer;
-        console.log('player: ', player);
-        if(!this.playerListService.isStartQuestion()){
-          this.playerListService.checkWIthLastPlayerName(player)? player.correct = true : player.correct = false;
+    sendToNG$.pipe(takeUntil(this.destroy$)).subscribe((value) => {
+      if (value) {
+        const player = {
+          name: value,
+          withoutVowelsName: removeVowels(value),
+        } as RulesPlayer;
+        if (!this.playerListService.isStartQuestion()) {
+          this.playerListService.checkWIthLastPlayerName(player)
+            ? (player.correct = true)
+            : (player.correct = false);
+          console.log();
         }
         this.playerListService.recivePlayerName.push(player);
         this.playerList$ = of(this.playerListService.recivePlayerName);
+        this._cdr.detectChanges();
       }
-    })
+    });
     this.myForm = new FormGroup({
-      playerName: new FormControl('', [Validators.required])
+      playerName: new FormControl('', [Validators.required]),
     });
   }
   update() {
-    if(this.myForm.valid){
+    if (this.myForm.valid) {
       sendToElm(this.myForm.value);
     }
   }
